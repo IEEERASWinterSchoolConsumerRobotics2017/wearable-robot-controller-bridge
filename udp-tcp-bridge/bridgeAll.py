bde16f70d17b8546ce6cb83bb029e5175393b5f3
Daniel Lofaro <dan@danlofaro.com>
2017-01-06 18:23:18 -0700

    running bridgeAll;

--- udp-tcp-bridge/bridgeAll.py
+++ udp-tcp-bridge/bridgeAll.py
@@ -3,7 +3,7 @@ import sys
 from thread import *
 import time
  
-HOST = ''   # Symbolic name meaning all available interfaces
+HOST = '10.18.81.7'   # Symbolic name meaning all available interfaces
 PORT = 8889 # Arbitrary non-privileged port
 PORT_HEART = 8890 # heart rate port
 PORT_LIGHTS = 8891
@@ -30,6 +30,10 @@ joy_right_y = 0.0
 message_left = 'left'
 message_right = 'right'
 message_joy = 'joy'
+
+light_bright = 0.0
+light_sat = 0.0
+light_hue = 0.0
 
 heart_rate = 0.0
 message_heart = 'heart'
@@ -104,7 +108,7 @@ def clientthread(conn):
         print data
         
         ds = data.split(" ")
-        if len(ds) > 0:
+        if len(ds) >= 2:
           if ds[0] == message_req:
             if ds[1] == message_joy:
               if len(ds) >= 3:
@@ -116,6 +120,68 @@ def clientthread(conn):
               if len(ds) >=3:
                 if ds[2] == message_rate:
                   sendHeartRate(conn)
+            if ds[1] == 'light':
+              if len(ds) >=3:
+                if ds[2] == 'bright':
+                  sendLightBright(conn)
+                if ds[2] == 'color':
+                  sendLightColor(conn)
+            if ds[1] == 'music':
+              if len(ds) >=3:
+                if ds[2] == 'genre':
+                  sendMusicGenre(conn)
+            if ds[1] == 'video':
+              if len(ds) >=3:
+                if ds[2] == 'genre':
+                  sendVideoGenre(conn)
+            if ds[1] == 'voice':
+              if len(ds) >=3:
+                if ds[2] == 'genre':
+                  sendVoiceGenre(conn)
+            if ds[1] == 'thermostat':
+              if len(ds) >=3:
+                if ds[2] == 'all':
+                  sendThermostatAll(conn)
+            if ds[1] == 'smoke':
+              if len(ds) >=3:
+                sendSmoke(conn)
+            #if ds[1] == 'camera':
+            if ds[1] == 'item':
+              if len(ds) >=3:
+                if ds[2] == 'des':
+                  sendItem(conn)
+          if ds[0] == 'set':
+           if len(ds) >=3:
+            if ds[1] == 'music':
+              if len(ds) >=4:
+                if ds[2] == 'genre':
+                  setMusic(conn,ds[3])
+            if ds[1] == 'video':
+              if len(ds) >=4:
+                if ds[2] == 'genre':
+                  setVideo(conn,ds[3])
+            if ds[1] == 'voice':
+              if len(ds) >=4:
+                if ds[2] == 'command':
+                  sendSetVoice(conn, ds[3])
+            if ds[1] == 'thermostat':
+              if len(ds) >=4:
+                if ds[2] == 'actual':
+                  setThermostatActual(conn, ds[3])
+                if ds[2] == 'des':
+                  setThermostatDes(conn, ds[3])
+            if ds[1] == 'smoke':
+              setSmoke(conn,ds[2])
+            if ds[1] == 'camera':
+              if len(ds) >=5:
+                if ds[2] == 'state':
+                  setCameraState(conn, ds[3], ds[4])
+                if ds[2] == 'image':
+                  setCameraImage(conn, ds[3])
+            if ds[1] == 'item':
+              if len(ds) >=4:
+                if ds[2] == 'des':
+                  setItemDes(conn, ds[3])
                           
         ##conn.sendall(robot_message)
         #conn.sendall(reply)
@@ -126,6 +192,104 @@ def clientthread(conn):
     conn.close()
  
 #now keep talking with the client
+def sendMusicGenre(conn):
+  reply = message_rep + ' ' + 'music' + ' ' + 'genre' + ' ' + music_genre
+  conn.sendall(reply)
+def sendVideoGenre(conn):
+  reply = message_rep + ' ' + 'video' + ' ' + 'genre' + ' ' + video_genre
+  conn.sendall(reply)
+
+def sendVoiceGenre(conn):
+  reply = message_rep + ' ' + 'voice' + ' ' + 'command' + ' ' + item_cmd
+  conn.sendall(reply)
+
+
+def sendThermostatAll(conn):
+  reply = message_rep + ' ' + 'thermostat' + ' ' + 'des' + ' ' + str(thermostat_des) + ' ' + str(thermostat_act)
+  conn.sendall(reply)
+
+def sendSmoke(conn):
+  reply = message_rep + ' ' + 'smoke' + ' ' + str(smoke_onoff)
+  conn.sendall(reply)
+
+def sendItem(conn):
+  reply = message_rep + ' ' + 'item' + ' ' + 'des' + ' ' + item_des
+  conn.sendall(reply)
+
+music_genre = 'rock'
+def setMusic(conn,):
+  global music_genre
+  music_genre = val
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+video_genre = 'aciton'
+def setVideo(conn, val):
+  global video_genre
+  video_genre = val
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+voice_cmd = 'none'
+def setVoice(conn,val):
+  global voice_cmd
+  voice_cmd = val
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+thermostat_act = 0.0
+def setThermostatActual(conn,val):
+  global thermostat_act
+  thermostat_act = float(val)
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+thermostat_des = 0.0
+def setThermostatDes(conn, val):
+  global thermostat_des
+  thermostat_des = float(val)
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+smoke_onoff = 0
+def setSmoke(conn, onoff):
+  global smoke_onoff
+  smoke_onoff = int(onoff)
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+img_name = 'none'
+img_time = 'none'
+def setCameraState(conn, nname, ttime):
+  global img_name
+  global img_time 
+  img_name = nname
+  img_time = ttime
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+img_str = 'none'
+def sSetCameraImage(conn, img):
+  global img_str
+  img_str = img
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+item_des = 'none'
+def setItemDes(conn, buff):
+  global item_des
+  item_des = buff
+  reply = message_rep + ' ' + 'ok'
+  conn.sendall(reply)
+
+
+def sendLightBright(conn):
+  reply = message_rep + ' ' + 'light' + ' ' + 'bright' + ' ' + str(light_bright)
+  conn.sendall(reply)
+
+def sendLightColor(conn):
+  reply = message_rep + ' ' + 'bright' + ' ' + 'color' + ' ' + str(light_hue) + ' ' + str(light_sat)
+  conn.sendall(reply)
 
 def sendHeartRate(conn):
   reply = message_rep + ' ' + message_rate + ' ' + str(heart_rate)
